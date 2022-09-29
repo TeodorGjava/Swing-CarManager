40b078305c246c288ce5bf3314c77091aa80d1db
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-09-29 14:48:01 +0300

    Create CarEvent.java

---
+++ src/models/CarEvent.java
@@ -0,0 +1,11 @@
+package models;
+
+public class CarEvent {
+    private String date;
+    private String event;
+    private int mileage;
+    private String invoice;
+    private String contractor;
+    private double price;
+
+}
