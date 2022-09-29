047b678de13e51786a4876f8c7702a37d26f9c5b
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-09-29 14:48:04 +0300

    Create Car.java

---
+++ src/models/Car.java
@@ -0,0 +1,29 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    private String brand;
+    private String model;
+    private List<CarEvent> eventList;
+
+    public Car(String brand, String model) {
+        setBrand(brand);
+        setModel(model);
+        this.eventList = new ArrayList<>();
+    }
+
+    private void setBrand(String brand){
+        if(brand.trim().isEmpty()){
+            throw new NullPointerException("Invalid brand");
+        }
+        this.brand=brand;
+    }
+    private void setModel(String model){
+        if(model.trim().isEmpty()){
+            throw new NullPointerException("Invalid model");
+        }
+        this.model=model;
+    }
+}
