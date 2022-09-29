09293273fc516d5bc068131720ffc6a8bc32f3a2
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-09-29 14:22:13 +0300

    Update ConverterGUI.java

--- src/ConverterGUI.java
+++ src/ConverterGUI.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.ActionListener;import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.text.DecimalFormat;
 
 public class ConverterGUI extends JFrame {
     private JPanel mainPanel;
@@ -9,7 +11,8 @@ public class ConverterGUI extends JFrame {
     private JButton convertButton;
     private JLabel answer;
     private static final double EUR= 1.95583;
-
+    private static final String pattern ="###.##";
+    private static final DecimalFormat df = new DecimalFormat(pattern);
     public ConverterGUI(String title) {
         super(title);
         //close operation
@@ -20,15 +23,29 @@ public class ConverterGUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 double lv = Double.parseDouble(input.getText());
-                double ans = lv/EUR;
-                answer.setText(String.valueOf(ans));
+                String result = df.format(lv/EUR);
+                answer.setText(result);
+            }
+        });
+    convertButton.addKeyListener(new KeyAdapter() { } );input.addKeyListener(new KeyAdapter() { } );input.addKeyListener(new KeyAdapter() { } );
+        input.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if(e.getKeyCode()==KeyEvent.VK_ENTER){
+                    double lv = Double.parseDouble(input.getText());
+                    String result = df.format(lv/EUR);
+                    answer.setText(result);
+                }
+                if(e.getKeyCode()==KeyEvent.VK_BACK_SPACE){
+                    answer.setText("");
+                }
             }
         });
     }
 
     public static void main(String[] args) {
         JFrame frame = new ConverterGUI("Кoлколатор");
-        frame.setSize(300,400);
+        frame.setSize(300,300);
         frame.setVisible(true);
     }
 }
