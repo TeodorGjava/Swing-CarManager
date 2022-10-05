9e88264b8f63b9ca088004ecd906cf398adea148
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-10-05 18:19:37 +0300

    table showing multiple and repeating car-repairs fixed

--- src/demo/MixedTable.java
+++ src/demo/MixedTable.java
@@ -10,7 +10,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class MixedTable {
     private JPanel panel1;
@@ -18,21 +17,23 @@ public class MixedTable {
     private JTable events;
     final String DB_URL = "jdbc:mysql://localhost/carbase";
     final String USERNAME = "root";
-    final String PASS = "root";
+    final String PASS = "root123";
     Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASS);
 
 
+        //TODO: Display table with events only for selected row from main table !
+        //Done !
     public MixedTable() throws SQLException {
         DefaultTableModel model = (DefaultTableModel) mixedTable.getModel();
         mixedTable.setAutoCreateColumnsFromModel(true);
         mixedTable.setAutoCreateRowSorter(true);
         mixedTable.setPreferredSize(new Dimension(200, 300));
-        model.addColumn("ID");
+
         model.addColumn("Brand");
         model.addColumn("Model");
 
         ArrayList<Car> carsList = getCars();
-        Object[] rows = new Object[3];
+        Object[] rows = new Object[2];
         for (Car car : carsList) {
             rows[0] = car.getBrand();
             rows[1] = car.getModel();
@@ -57,17 +58,26 @@ public class MixedTable {
         mixedTable.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                DefaultTableModel dtm = (DefaultTableModel) mixedTable.getModel();
-                int rowSelected = mixedTable.getSelectedRow();
-                String carID = String.valueOf(dtm.getValueAt(rowSelected, 1));
                 try {
-                    events.setModel(displayTable(carID));
+                    getSelectedCarInfo();
                 } catch (SQLException ex) {
                     ex.printStackTrace();
                 }
-
             }
         });
+    }
+
+    private void getSelectedCarInfo() throws SQLException {
+        clearTable();
+        DefaultTableModel dtm = (DefaultTableModel) mixedTable.getModel();
+        DefaultTableModel eventsModel = (DefaultTableModel) events.getModel();
+        eventsModel.setRowCount(0);
+        int rowSelected = mixedTable.getSelectedRow();
+        String carID = String.valueOf(dtm.getValueAt(rowSelected, 1));
+        events.setModel(displayTable(carID));
+    }
+
+    private void clearTable() {
     }
 
     public DefaultTableModel displayTable(String carModel) throws SQLException {
@@ -101,7 +111,6 @@ public class MixedTable {
             //Table displaying event info price with/without taxes completed
             model2.addRow(new Object[]{model, event, date, contractor, invoice, mileage, price, priceWithTaxes});
         }
-        //TODO: Display table with events only for selected row from main table !
 
 
         return model2;
